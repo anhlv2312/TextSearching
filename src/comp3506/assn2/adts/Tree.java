b5416d15cbd9c54052bd742bce7afc5d10926b80
Vu Anh LE <anhlv2312@gmail.com>
2018-09-21 12:24:10 +1000

    add Abstract Tree class

--- src/comp3506/assn2/adts/Tree.java
+++ src/comp3506/assn2/adts/Tree.java
@@ -2,13 +2,40 @@ package comp3506.assn2.adts;
 
 import java.util.Iterator;
 
+/* [1 pp. 313] */
 public interface Tree<E> extends Iterable<E> {
 
-//    Position<E> root();
-//    Position<E> parent(Position<E> p) throws IllegalArgumentException; Iterable<Position<E>> children(Position<E> p)
-//            throws IllegalArgumentException; int numChildren(Position<E> p) throws IllegalArgumentException;
-//    boolean isInternal(Position<E> p) throws IllegalArgumentException; boolean isExternal(Position<E> p) throws IllegalArgumentException; boolean isRoot(Position<E> p) throws IllegalArgumentException; int size();
-//    boolean isEmpty();
-//    Iterator<E> iterator(); Iterable<Position<E>> positions();
+    Position<E> root();
+
+    Position<E> parent(Position<E> p) throws IllegalArgumentException;
+
+    Iterable<Position<E>> children(Position<E> p) throws IllegalArgumentException;
+
+    int numChildren(Position<E> p) throws IllegalArgumentException;
+
+    boolean isInternal(Position<E> p) throws IllegalArgumentException;
+
+    boolean isExternal(Position<E> p) throws IllegalArgumentException;
+
+    boolean isRoot(Position<E> p) throws IllegalArgumentException;
+
+    int size();
+
+    boolean isEmpty();
+
+    Iterator<E> iterator();
+
+    Iterable<Position<E>> positions();
+
+    interface Position<E> {
+        E getElement();
+    }
 
 }
+
+/*
+ * REFERENCE
+ * [1] M. T. Goodrich, R. Tamassia, and M. H. Goldwasser, Data
+ * structures and algorithms in Java. John Wiley & Sons, 2014.
+ *
+ */
